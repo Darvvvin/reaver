a3ebe16eca715791599b3f05443bc301c54a9707
Jacob Darvin <jacob_darvin@dlsu.edu.ph>
2022-06-05 14:35:48 +0800

    yea

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Intro from './components/Intro/Intro.jsx'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
+}
+
+export default App;
